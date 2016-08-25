b312854422127197eb30d7f38874927ed93a8ab4
MartinKilonzo <martinkilonzo2@gmail.com>
2016-08-24 21:41:39 -0400

    Removed: unnecessary code.

--- src/components/Tickets/TicketGroupView.jsx
+++ src/components/Tickets/TicketGroupView.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Col, Button, ButtonGroup, Row} from 'react-bootstrap';
 
+import colors from '../Home/colors.jsx';
+import PageHeader from '../Home/PageHeader.jsx';
 import TicketGroupMenu from './TicketGroupMenu.jsx'
 import ListTicketGroups from './ListTicketGroups.jsx';
-import colors from '../Home/colors.jsx';
 
 class TicketGroupView extends React.Component {
   constructor(props) {
@@ -14,17 +15,9 @@ class TicketGroupView extends React.Component {
     //TODO: Fix first ticketpost being listed last --> sort tickets client side or via API
     return (
       <div>
-        <div className="index">
-          <div className="notice">
-            <h2>Ticket Groups</h2>
-          </div>
-        </div>
-        <Col xs={3}>
-          <TicketGroupMenu setFilter={this.setTicketFilter} resetFilter={this.resetFilter} ticketPosts={this.props.ticketPosts}></TicketGroupMenu>
-        </Col>
-        <Col xs={9}>
-          <ListTicketGroups ticketPosts={this.props.ticketPosts}></ListTicketGroups>
-        </Col>
+        <PageHeader align='right'>Tickets</PageHeader>
+        <TicketGroupMenu setFilter={this.setTicketFilter} resetFilter={this.resetFilter} ticketPosts={this.props.ticketPosts}></TicketGroupMenu>
+        <ListTicketGroups ticketPosts={this.props.ticketPosts}></ListTicketGroups>
       </div>
     );
   }
